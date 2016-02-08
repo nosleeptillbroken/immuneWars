1085998e51f07df0fc5e711fbb96e857b9f474d3
J T <fff4word@live.ca>
2016-02-08 12:16:04 -0500

    Add changing color for health bars

--- Assets/Scripts/Creep.cs
+++ Assets/Scripts/Creep.cs
@@ -8,7 +8,9 @@ public class Creep : MonoBehaviour
 {
     //Vars for the health bar
     [SerializeField] private GameObject healthBar; //link to the health bar game object
-    [SerializeField] private Transform lookHere;
+    private Quaternion healthBarRot; //var for storing the initial rotation of the health bar
+    private MeshRenderer healthBarRenderer; //for enabling and disabling renderer
+    private Vector3 healthBarScale; //to scale properly
 
     // Target for despawning
     private Transform target;
@@ -20,6 +22,7 @@ public class Creep : MonoBehaviour
     /// How much health the unit currently has.
     /// </summary>
 	public int Health = 50;
+    private int startingHealth;
 
     /// <summary>
     /// How much damage the unit inflicts when it successfully leaks through.
@@ -31,6 +34,13 @@ public class Creep : MonoBehaviour
     {
         // The creep is referenced with the NavMesh so it can interact.
         agent = GetComponent<NavMeshAgent>();
+
+        //Store the initial rotation of the health bar
+        healthBarRot = healthBar.transform.rotation;
+        healthBarScale = healthBar.transform.localScale;
+        healthBarRenderer = GetComponentsInChildren<MeshRenderer>()[1]; //associate renderer for healthbar
+        healthBarRenderer.enabled = false; //make invisible from start
+        startingHealth = Health; //store full health value
 
         // Creeps are instantiated each wave, and references are made to the despawn point for pathing.
         GameObject despawnGameObject = GameObject.FindWithTag("Despawn");
@@ -52,24 +62,36 @@ public class Creep : MonoBehaviour
     /// <param name="damage">How much damage the enemy will take</param>
 	void TakeDamage(int damage)
     {
+        if (startingHealth == Health)
+        {
+            healthBarRenderer.enabled = true; //begin rendering health bar once creep takes damage for first time
+        }
+       
+        // lower health
 		Health -= damage;
 		if (Health <= 0)
         {
 			Destroy(gameObject);
 		}
+
+       //rescale healthBar
+        Vector3 newScale = new Vector3(((healthBarScale.x * Health) / startingHealth), healthBarScale.y, healthBarScale.z);
+        healthBar.transform.localScale = newScale;
 	}
 
     // Update
 	void Update ()
     {
-        //make the healthbar look at the camera
-
-        transform.LookAt(lookHere);
-
         if (agent && target)
         {
             agent.SetDestination(target.position);
         }
+    }
+
+    // LateUpdate
+    void LateUpdate ()
+    {
+        healthBar.transform.rotation = healthBarRot; //rotation correction for health bar
     }
 
     // OnTriggerEnter
