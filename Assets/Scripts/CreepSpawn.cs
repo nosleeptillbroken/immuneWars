89cfd16e66fe8e5cb7dc8d5b9e6b642f41b5a09c
J T <fff4word@live.ca>
2016-02-29 16:05:05 -0500

    Add materials for differentiating creep types. Add creep types with different health, damage, speed, size, accelleration. Changed spawner script to run following levelList, then waveList, then creepList allowing each level, wave, and number of creeps of each type within wave to be adjusted.

--- Assets/Scripts/CreepSpawn.cs
+++ Assets/Scripts/CreepSpawn.cs
@@ -4,13 +4,26 @@
 using UnityEngine;
 using System.Collections;
 
-public class CreepSpawn : MonoBehaviour {
-	public GameObject creep;
+[System.Serializable]
+public class CreepPair // provides a pair of GameObject to select creep type and number of that type to spawn
+{
+    public GameObject creep;
+    public int number = 1; // should initialize to 1, but it doesnt seem to
+}
 
-	public int creepCount;
-	public float spawnWait;
-	public float startWait;
-	public float waveWait;
+[System.Serializable]
+public class WaveList // allows for the creation of a 2 dimensional array of CreepPairs 
+{
+    public CreepPair[] waveList;
+}
+
+public class CreepSpawn : MonoBehaviour {
+    public WaveList[] levelList; // an array of waves will create the level
+
+	// public int creepCount;
+	public float spawnWait; // time to wait between spawns
+	public float startWait; // time to wait before first spawn of level
+	public float waveWait; // time to wait in between waves
 
 	void Start ()
 	{
@@ -20,16 +33,22 @@ public class CreepSpawn : MonoBehaviour {
 	IEnumerator SpawnWaves ()
 	{
 		yield return new WaitForSeconds (startWait);
-		while (true)
-		{
-			for (int i = 0;i < creepCount; i++) 
-			{
-				Vector3 spawnPosition = transform.position;
-				Quaternion spawnRotation = Quaternion.identity;
-				Instantiate (creep, spawnPosition, spawnRotation);
-				yield return new WaitForSeconds (spawnWait);
-			}
-			yield return new WaitForSeconds (waveWait);
-		}
+        for (int i = 0; i < levelList.Length; i++) // for every wave in the level
+        {
+            for (int j = 0; j < levelList[i].waveList.Length; j++) // for every creep type listed in the wave
+            {
+                for (int k = 0; k < levelList[i].waveList[j].number; k++) // for every creep of that type in the wave
+                {
+                    Vector3 spawnPosition = transform.position;
+                    Quaternion spawnRotation = Quaternion.identity;
+                    Instantiate (levelList[i].waveList[j].creep, spawnPosition, spawnRotation); // spawn wave for this wave, of this type
+                    yield return new WaitForSeconds (spawnWait); // wait before spawning next creep
+                }
+            }
+            yield return new WaitForSeconds(waveWait); // wait before starting next wave
+        }
+        // fall out the bottom
+        Debug.Log("Level Ended"); // log that level is over, will need a call for level completed scene to begin
+        // Debug.Break();
 	}
 }
