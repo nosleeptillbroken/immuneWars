0589df66b79d735ff96aac1b82e7867601325a9d
Andrew MacMillan <andrewmacmilan1@hotmail.com>
2016-03-02 18:56:59 -0500

    commented code

--- Assets/Scripts/DestroyAfterTime.cs
+++ Assets/Scripts/DestroyAfterTime.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class DestroyAfterTime : MonoBehaviour {
     public float time;
-	// Use this for initialization
+	//Universal script to destroy objects after a set time. you can define the time in the inspector
 	void Start () {
         StartCoroutine(deathClock());
 	}
