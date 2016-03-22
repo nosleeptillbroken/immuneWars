59c4799659bb54bd6eeb08352f78883a9a4651d1
J T <fff4word@live.ca>
2016-03-22 12:20:16 -0400

    Adds functionality to LangData class Adds UseLang class which can be added to Text objects, allowing them to receive contents from language file. Will add language changing functions to LangData

--- Assets/Scripts/Languages/LangData.cs
+++ Assets/Scripts/Languages/LangData.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 
 public class LangData {
     private static LangData instance = null;
-    private string[] langPaths = {"../Languages/eng.iwl", "../Languages/frn.iwl"};
-    private string langPath = langPaths[0];
+    private string[] langPaths = {"./Assets/Languages/eng.iwl", "../Languages/frn.iwl"};
+    private string langPath;
 
     public Dictionary<string, string> current;
 
     // Loads default language
     private LangData()
     {
+        current = new Dictionary<string, string>();
+        langPath = langPaths[0];
         LoadLang();
     }
 
+    // ensures one and only one instance of LangData
     public static LangData Instance
     {
         get 
@@ -25,18 +29,47 @@ public class LangData {
         }
     }
 
+    // Reads selected language file
+    // Splits line entries into keys and values
+    // Stores the results in Dictionary current
     private void LoadLang()
     {
         string line;
+        string key;
+        string value;
 
-        // Read the file and display it line by line.
-        System.IO.StreamReader file = 
-            new System.IO.StreamReader(@langPath);
+        int splitIndex;
+
+        StreamReader file = 
+            new StreamReader(@langPath);
         while((line = file.ReadLine()) != null)
         {
             Debug.Log(line);
+            splitIndex = line.IndexOf('=', 1);
+            Debug.Log(splitIndex);
+            if (splitIndex >= 1)
+            {
+                key = line.Substring(0, splitIndex);
+                value = line.Substring(splitIndex + 1, line.Length - (splitIndex + 1));
+                Debug.Log(key + "," + value);
+                Debug.Log(this.current);
+                this.current.Add(key, value);
+            }
         }
 
         file.Close();
+    }
+
+    public string Retrieve (string getKey)
+    {
+        string value;
+        if (current.TryGetValue(getKey, out value))
+        {
+            return value;
+        }
+        else
+        {
+            return "Not Found";
+        }
     }
 }
---
+++ Assets/Scripts/Languages/UseLang.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UseLang : MonoBehaviour {
+    public string langKey;
+
+    private Text txt;
+
+    void Awake()
+    {
+        txt = gameObject.GetComponent<Text>();
+        txt.text = LangData.Instance.Retrieve(langKey);
+    }
+}
