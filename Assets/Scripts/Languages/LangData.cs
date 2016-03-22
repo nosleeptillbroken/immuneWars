91070e94e95c7d606d9e1d41f8a46af37251828b
J T <fff4word@live.ca>
2016-03-21 20:10:03 -0400

    Add LangData class file in Scripts/Languages folder. LangData will contain Dictionary filled with contents from language resource file for easy access LangData will also contain functions for interacting with class.

---
+++ Assets/Scripts/Languages/LangData.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LangData {
+    private static LangData instance = null;
+    private string[] langPaths = {"../Languages/eng.iwl", "../Languages/frn.iwl"};
+    private string langPath = langPaths[0];
+
+    public Dictionary<string, string> current;
+
+    // Loads default language
+    private LangData()
+    {
+        LoadLang();
+    }
+
+    public static LangData Instance
+    {
+        get 
+        {
+            if (instance == null)
+                instance = new LangData();
+            return instance;
+        }
+    }
+
+    private void LoadLang()
+    {
+        string line;
+
+        // Read the file and display it line by line.
+        System.IO.StreamReader file = 
+            new System.IO.StreamReader(@langPath);
+        while((line = file.ReadLine()) != null)
+        {
+            Debug.Log(line);
+        }
+
+        file.Close();
+    }
+}
